ea4e148c027ec72e342441df879d38c604e2e000
Saivijaay VinothKannaa <91886726+saivijaay2107@users.noreply.github.com>
2022-09-06 11:51:23 +0530

    Add files via upload
    Dot net backend files were added with Log4Net

---
+++ Project/Migrations/20220902042620_first.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Project2.Migrations
+{
+    public partial class first : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Designations",
+                columns: table => new
+                {
+                    DesignationID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DesignationName = table.Column<string>(nullable: true),
+                    RoleName = table.Column<string>(nullable: true),
+                    DepartmentName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Designations", x => x.DesignationID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Interns",
+                columns: table => new
+                {
+                    InternId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    InternName = table.Column<string>(nullable: true),
+                    EmailId = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    DesignationName = table.Column<string>(nullable: true),
+                    DesignationRole = table.Column<string>(nullable: true),
+                    Qualification = table.Column<string>(nullable: true),
+                    Company_Hours = table.Column<string>(nullable: true),
+                    Intern_hours = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Interns", x => x.InternId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Designations");
+
+            migrationBuilder.DropTable(
+                name: "Interns");
+        }
+    }
+}
